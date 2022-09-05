3e9b30d5206bb7fd94a2b344ab607b0c8eb73453
Kouji Kozaki <kozaki@osakac.ac.jp>
2022-09-05 16:10:21 +0900

    タクソン用サンプル

---
+++ options.js
@@ -0,0 +1,77 @@
+let search_cond = 
+[
+  {
+    "id": "cond1",
+    "ctext": " 分類[wdt:P31]＝タクソン",
+    "cond": "wdt:P31",
+    "val": "wd:Q16521",
+    "type": "ID",
+    "const": true
+  },
+  {
+    "id": "cond2",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond3",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond4",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond5",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  }
+];
+
+let search_prop = 
+[
+  {
+    "id": "opt1",
+    "prop": "",
+    "pname": "",
+    "optional": false
+  },
+  {
+    "id": "opt2",
+    "prop": "",
+    "pname": "",
+    "optional": false
+  },
+  {
+    "id": "opt3",
+    "prop": "",
+    "pname": "",
+    "optional": false
+  },
+  {
+    "id": "opt4",
+    "prop": "",
+    "pname": "",
+    "optional": false
+  },
+  {
+    "id": "opt5",
+    "prop": "",
+    "pname": "",
+    "optional": false
+  }
+];
